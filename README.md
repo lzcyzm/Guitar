c928e79d67c3e361903582833929e15d810090ce
j.meng <j.meng@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-10-20 03:56:12 +0000

    version 1.3.2
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_2/madman/Rpacks/Guitar@109781 bc3139a8-67e5-0310-9ffc-ced21a209358

--- README.md
+++ README.md
@@ -1,20 +1,8 @@
 
-# Guitar R package
+# Guitar R/Bioconductor package
 
-Guitar: annotation guided transcriptomic analysis of genomic features
+gene annotation guided transcriptomic analysis of RNA-related genomic features
 
-***
+Check more information at: http://bioconductor.org/packages/Guitar/
 
-The latest help manual and an introduction are provided with the package
-
-Please contact jia.meng@hotmail.com for questions and suggestions.
-
-***
-
-Obsolete versions and their manuals are available here: 
-
-http://rpubs.com/lzcyzm/travis3
-
-http://rpubs.com/lzcyzm/travis1
-
-http://rpubs.com/lzcyzm/travis0
+Please contact jia.meng@hotmail.com if you have any question or suggestion.
--- inst/extdata/others/mightUseful.R
+++ inst/extdata/others/mightUseful.R
@@ -1,13 +1,18 @@
+# prepare the txdb file
 gr <- import("hg19_toy.gtf",asRangedData = FALSE)
 hg19_toy <- makeTxDbFromGRanges(gr)
 saveDb(hg19_toy, "hg19_toy.sqlite")
 hg19_toy <- loadDb("hg19_toy.sqlite")
 
+gr <- import("mm10_chr1_part.gtf")
+mm10_toy <- makeTxDbFromGRanges(gr)
+saveDb(mm10_toy, "mm10_toy.sqlite")
+mm10_toy <- loadDb("mm10_toy.sqlite")
 
-
-
+# package
 package.skeleton(name="Travis",code_files =list.files())
 
+# prepare other data sets
 a <- read.table("cellular_dox_macs_kpdup_peaks.narrowPeak",sep="\t",header=FALSE)
 write.table(a[1:1000,],file= "m6A_hg19_1000peaks_macs2.narrowPeak",sep="\t",
             quote = FALSE, row.names = FALSE,
@@ -23,3 +28,12 @@ a <- read.table("mmRNA.bismark.cov",sep="\t",header=FALSE)
 write.table(a[1:10000,],file= "RNAm5C_mm10_10000sites_bismark.cov",sep="\t",
             quote = FALSE, row.names = FALSE,
             col.names = FALSE)
+
+
+# prepare the bam file
+samtools index 866991.bam
+samtools index 866992.bam
+samtools view -b 866991.bam chr1:6,177,566-7,207,283 -o 866991_part.bam
+samtools view -b 866992.bam chr1:6,177,566-7,207,283 -o 866992_part.bam
+
+7,236,448
